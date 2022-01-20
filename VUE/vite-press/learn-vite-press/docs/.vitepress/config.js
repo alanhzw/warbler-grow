52e57012b9b3f782b0d29c997b425fd5ae2209b4
韩志伟 <hanzhiwei@lucidata.cn>
2022-01-20 14:42:49 +0800

    vite-press

---
+++ VUE/vite-press/learn-vite-press/docs/.vitepress/config.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: 一尾流莺
+ * @Description:vite-press配置文件
+ * @Date: 2022-01-20 14:29:34
+ * @LastEditTime: 2022-01-20 14:32:08
+ * @FilePath: \learn-vite-press\docs\.vitepress\config.js
+ */
+module.exports = {
+  base: '/base/', // 简单说如下配置就会让你的项目的路径从 `https://domain.com/` 变成 `https://domain.com/base/`
+  lang: 'en-US', // 页面 HTML 的 lang 属性 `<html lang="en-US">`
+  title: '测试标题', // 页面标题，也会显示在 navbar 上
+  description: 'A VitePress site' // 渲染成页面的 `<meta name="description" content="A VitePress site">`
+}
--- VUE/vite-press/learn-vite-press/docs/index.md
+++ VUE/vite-press/learn-vite-press/docs/index.md
@@ -2,6 +2,8 @@
  * @Author: 一尾流莺
  * @Description:
  * @Date: 2022-01-20 14:22:57
- * @LastEditTime: 2022-01-20 14:22:59
+ * @LastEditTime: 2022-01-20 14:33:50
  * @FilePath: \learn-vite-press\docs\index.md
 -->
+
+# Hello VitePress1
